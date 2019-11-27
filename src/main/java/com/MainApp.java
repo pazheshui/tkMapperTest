d2a6a1438316de7fd76128c9e747833939bf3924
pazheshui <424200659@qq.com>
2019-11-27 21:22:34 +0800

    通用mapper 小案例

--- src/main/java/com/MainApp.java
+++ src/main/java/com/MainApp.java
@@ -2,7 +2,7 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//测试
 @SpringBootApplication
 public class MainApp {
     public static void main(String[] args) {
